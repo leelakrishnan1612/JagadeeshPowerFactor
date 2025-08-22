cfc296cf897a6c7350023dc0960ad7548169fcac
leelakrishnan1612 <leelakrishnan1612@gmail.com>
2025-08-22 10:43:36 +0530

    d

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/JagadeeshPowerFactor/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/JagadeeshPowerFactor/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
